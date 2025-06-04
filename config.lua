7f00e4a891daf73f9e0a9a57a8acf77f19e2bebf
Jeuriz <jeuriz@live.com>
2025-06-04 01:48:10 -0400

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+Config.Commands = {
+    openLeaderboard = 'zombieleaderboard', -- Comando para abrir el leaderboard
+    resetStats = 'resetzombiestats' -- Comando para resetear estadísticas
+}
+
+Config.Permissions = {
+    reset = 'admin' -- Permiso necesario para resetear stats
+}
+
+Config.DefaultIcon = '🧟' -- Icono por defecto
+Config.MaxDisplayPlayers = 10 -- Máximo de jugadores a mostrar
