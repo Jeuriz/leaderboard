d0b935afe96c763faa6e8543126ca7b4978a1125
Jeuriz <jeuriz@live.com>
2025-06-04 01:47:57 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,29 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Tu Nombre'
+description 'Zombie Kills Leaderboard para QBCore'
+version '1.0.0'
+
+shared_scripts {
+    '@qb-core/shared/locale.lua',
+    'config.lua'
+}
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    'server/main.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
+
+lua54 'yes'
